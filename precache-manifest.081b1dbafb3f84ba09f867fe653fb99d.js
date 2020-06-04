a96ae9cb52cbb652cb7d778c91adfa4615a27d79
chrismw <chrismw7579@gmail.com>
2020-06-03 23:40:30 -0700

    Updates

---
+++ precache-manifest.081b1dbafb3f84ba09f867fe653fb99d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c414ca62c9efee8533f8e06581a23b7a",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "c119647cdc77ebbcaba7",
+    "url": "/portfolioreact/static/css/main.93b80b8c.chunk.css"
+  },
+  {
+    "revision": "e90871040b5888d139dd",
+    "url": "/portfolioreact/static/js/2.eeddefbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioreact/static/js/2.eeddefbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c119647cdc77ebbcaba7",
+    "url": "/portfolioreact/static/js/main.1145d7a1.chunk.js"
+  },
+  {
+    "revision": "a34cc660fa13b3a572eb",
+    "url": "/portfolioreact/static/js/runtime-main.ef291dda.js"
+  },
+  {
+    "revision": "305eef83a3f4d0ed9cdec8b20eb60111",
+    "url": "/portfolioreact/static/media/Employeetracker.305eef83.png"
+  },
+  {
+    "revision": "7b5161dfee1b7be8b733317c84506382",
+    "url": "/portfolioreact/static/media/MyPhoto.7b5161df.jpg"
+  },
+  {
+    "revision": "5cd27cb4815f30aec9bc02c89476ffeb",
+    "url": "/portfolioreact/static/media/Resume.5cd27cb4.pdf"
+  },
+  {
+    "revision": "879c2a51f89787baaf7df147b8f195a3",
+    "url": "/portfolioreact/static/media/newsflash.879c2a51.png"
+  },
+  {
+    "revision": "1d80c4008d49b1281c97049695832fd8",
+    "url": "/portfolioreact/static/media/planner.1d80c400.png"
+  },
+  {
+    "revision": "61f8eefc1de69d0f63e23fe4d71cd6b2",
+    "url": "/portfolioreact/static/media/quiz.61f8eefc.png"
+  },
+  {
+    "revision": "f364279ce61c3a745e7811d04ff16d02",
+    "url": "/portfolioreact/static/media/radar.f364279c.png"
+  },
+  {
+    "revision": "420e2fd17b67d34ec1b43a94e3bda7bd",
+    "url": "/portfolioreact/static/media/reactDirectory.420e2fd1.png"
+  },
+  {
+    "revision": "b6a328e8cba2d08739aa7a4f7ee21fb0",
+    "url": "/portfolioreact/static/media/rendezvuous.b6a328e8.jpg"
+  },
+  {
+    "revision": "0aa999aafe669ef7230a5a76263f857d",
+    "url": "/portfolioreact/static/media/weather.0aa999aa.png"
+  },
+  {
+    "revision": "9b284953ac682a4789e5a0b3f115e64b",
+    "url": "/portfolioreact/static/media/whirlpool.9b284953.png"
+  }
+]);
